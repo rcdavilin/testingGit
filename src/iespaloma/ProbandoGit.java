75b9f622a00425ccbe9354eb54795dc8d9b955ee
SebastianCastanoSuarez1 <samesco323@gmail.com>
2023-04-25 14:26:07 +0200

    ILlo

--- src/iespaloma/ProbandoGit.java
+++ src/iespaloma/ProbandoGit.java
@@ -2,6 +2,6 @@ package iespaloma;
 
 public class ProbandoGit {
 	public static void main(String[] args) {
-		System.out.println("Hola 1");
+		System.out.println("Hola 1, pues ahora hola 2");
 	}
 }
